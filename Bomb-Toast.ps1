adb521f6a5550508593e862174d53c99188dd162
smooochy <dannysomogyi@gmail.com>
2022-06-13 08:04:38 -0500

    Changed hardcoded $TotalSeconds to accept parameter, fixed progress bar

--- Bomb-Toast.ps1
+++ Bomb-Toast.ps1
@@ -1,24 +1,26 @@
-$TotalSeconds = 5
+param($TotalSeconds = 30)
 $TimeLeft = $TotalSeconds
 
 $FirstDataBinding = @{
-    FirstLine = "Your Computer Will Self-Destruct in 30 Seconds. "
+    FirstLine = "Your Computer Will Self-Destruct in $TimeLeft Seconds. "
     SecondLine = "Please save your work"
     ThirdLine = "Fuse Lit"
     ProgressStatus = 'Fuse Progress'
     ProgressValue = $TimeLeft/$TotalSeconds
     ProgressDisplay = "$TimeLeft/$TotalSeconds"
 }
+$Progress = New-BTProgressBar -Status "ProgressStatus" -Value 'ProgressValue' -ValueDisplay 'ProgressDisplay'
 New-BurntToastNotification -Text "FirstLine", "SecondLine", "ThirdLine" -UniqueIdentifier "ToastBomb" -ProgressBar $Progress -AppLogo (New-BTImage -Source 'https://freesvg.org/img/bomb.png') -DataBinding $FirstDataBinding
 While($TimeLeft -ge 0){
     $DataBinding = @{
-        FirstLine = "Your Computer Will Self-Destruct in 30 Seconds. "
+        FirstLine = "Your Computer Will Self-Destruct in $TimeLeft Seconds. "
         SecondLine = "Please save your work"
         ThirdLine = "Fuse Lit"
         ProgressStatus = 'Fuse Progress'
         ProgressValue = $TimeLeft/$TotalSeconds
         ProgressDisplay = "$TimeLeft/$TotalSeconds"
     }
+    $Progress = New-BTProgressBar -Status "ProgressStatus" -Value 'ProgressValue' -ValueDisplay 'ProgressDisplay'
     Update-BTNotification -UniqueIdentifier "ToastBomb" -DataBinding $DataBinding
     $TimeLeft = $TimeLeft - 1
     Start-Sleep -Seconds 1
